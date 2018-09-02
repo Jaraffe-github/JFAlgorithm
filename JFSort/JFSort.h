74c7336bb810355a97b75f1c944aff008cb88220
SeungMinChoi <ccm1333@naver.com>
2018-09-02 22:44:09 +0900

    add Sort
    yet, if Compare is very slow, std::sort win

---
+++ JFSort/JFSort.h
@@ -0,0 +1,99 @@
+#pragma once
+
+namespace JFFoundation
+{
+    template <typename Value>
+    struct JFDefaultLessComparator
+    {
+        __forceinline bool operator () (const Value& lhs, const Value& rhs) const
+        {
+            return lhs < rhs;
+        }
+    };
+
+    template <typename Value>
+    struct JFDefaultLessComparator<Value*>
+    {
+        __forceinline bool operator () (const Value* lhs, const Value* rhs) const
+        {
+            return (*lhs) < (*rhs);
+        }
+    };
+
+    template <typename T>
+	__forceinline void Swap(T& lhs, T& rhs)
+    {
+        T temp = lhs;
+        lhs = rhs;
+        rhs = temp;
+    }
+
+	template <typename Value, typename Comparator = JFDefaultLessComparator<Value>>
+	void InsertionSort(Value* dataSet, size_t startIndex, size_t endIndex, Comparator& comparator)
+	{
+		Value temp;
+
+		for (size_t i = startIndex+1; i <= endIndex; ++i)
+		{
+			temp = dataSet[i];
+			size_t j = i;
+			while (j > startIndex && comparator(temp, dataSet[j-1]))
+			{
+				dataSet[j] = dataSet[j-1];
+				--j;
+			}
+			dataSet[j] = temp;
+		}
+	}
+
+    template <typename Value, typename Comparator = JFDefaultLessComparator<Value>>
+    size_t Partition(Value* dataSet, size_t startIndex, size_t endIndex, Comparator& comparator)
+    {
+		size_t pivot = endIndex;
+        size_t count = endIndex - startIndex;
+        size_t midIndex = startIndex + (count >> 1);
+
+        Swap(dataSet[pivot], dataSet[midIndex]);
+		
+        size_t leftPos = startIndex;
+        size_t rightPos = endIndex-1;
+		while (true)
+		{
+			while (comparator(dataSet[leftPos], dataSet[pivot]))
+				++leftPos;
+        
+			while (comparator(dataSet[pivot], dataSet[rightPos]))
+				--rightPos;
+        
+            if (leftPos >= rightPos)
+                break;
+			
+            Swap(dataSet[leftPos], dataSet[rightPos]);
+        
+            ++leftPos;
+            --rightPos;
+		}
+		Swap(dataSet[leftPos], dataSet[pivot]);
+
+		return leftPos;
+    }
+
+    template <typename Value, typename Comparator = JFDefaultLessComparator<Value>>
+    void Sort(Value* dataSet, size_t startIndex, size_t endIndex, Comparator comparator = Comparator())
+    {
+        if (startIndex >= endIndex)
+            return;
+
+		size_t count = endIndex - startIndex;
+		if (count < 128)
+		{
+		    InsertionSort(dataSet, startIndex, endIndex, comparator);
+		}
+		else
+		{
+			size_t pivot = Partition(dataSet, startIndex, endIndex, comparator);
+            Sort(dataSet, startIndex, pivot - 1, comparator);
+            Sort(dataSet, pivot + 1, endIndex, comparator);
+		}
+    }
+}
---
+++ JFSort/main.cpp
@@ -0,0 +1,90 @@
+#include "JFSort.h"
+
+#include <iostream> 
+#include <algorithm>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <chrono>
+
+class SimpleTimer
+{
+public:
+    using Clock = std::chrono::system_clock;
+    using Tick = std::chrono::time_point<Clock>;
+
+    void Start()
+    {
+        startTime = Clock::now();
+    }
+
+    double ElapsedTime()
+    {
+        Tick endTime = Clock::now();
+        return std::chrono::duration<double>(endTime - startTime).count();
+    }
+
+private:
+    Tick startTime;
+};
+
+using namespace JFFoundation;
+
+const size_t TestDataSetCount = 10000000ull;
+
+int main()
+{
+    SimpleTimer timer;
+    double mySortUsageTime = 0.0;
+    double stlSortUsageTime = 0.0;
+
+    std::vector<int> testDataSet1;
+    std::vector<int> testDataSet2;
+    testDataSet1.reserve(TestDataSetCount);
+    testDataSet2.reserve(TestDataSetCount);
+
+    // Init
+    for (int i = 0; i < TestDataSetCount; ++i)
+    {
+        testDataSet1.push_back(i);
+    }
+    std::random_shuffle(testDataSet1.begin(), testDataSet1.end());
+    testDataSet2 = testDataSet1;
+
+    // mySort
+    timer.Start();
+    Sort(&testDataSet1[0], 0ull, testDataSet1.size()-1);
+    mySortUsageTime = timer.ElapsedTime();
+
+    // stlSort
+    timer.Start();
+    std::sort(testDataSet2.begin(), testDataSet2.end());
+    stlSortUsageTime = timer.ElapsedTime();
+
+    // check
+    for (int i = 0; i < TestDataSetCount; ++i)
+    {
+        if (testDataSet1[i] != i)
+        {
+            std::cout << "SortFail" << std::endl;
+            std::cout << "Index : " << i << std::endl;
+            std::cout << "Number : " << testDataSet1[i] << std::endl;
+        }
+    }
+
+    // Print UsageTime
+    std::cout << "MySort UsageTime : " << mySortUsageTime << std::endl;
+    std::cout << "stlSort UsageTime : " << stlSortUsageTime << std::endl;
+
+    std::cout << std::endl;
+
+    // what is winner?
+    if (mySortUsageTime > stlSortUsageTime)
+        std::cout << "Winner : " << "STLSort" << std::endl;
+    else
+        std::cout << "Winner : " << "MySort" << std::endl;
+
+    getchar();
+
+    return 0;
+}
